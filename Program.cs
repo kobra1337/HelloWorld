3ce3eb97db2fb6213c16e8734d837a4c6f85e3d5
Jim Stahl <jstahl6387@gmail.com>
2020-03-26 19:22:46 -0500

    Terminal display message changed
    I changed the display message to be my name.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Jim Stahl");
             Console.Write("\r\nPress enter to exit...");
             Console.ReadLine();
         }
